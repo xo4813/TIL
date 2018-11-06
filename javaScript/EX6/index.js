ae5847d2f0bef494037b0dc2223f40be0ba8c4b7
taehan <tjxogks@naver.com>
2018-11-06 16:24:35 +0900

    EX6 start

---
+++ javaScript/EX6/index.js
@@ -0,0 +1,67 @@
+[].forEach()
+
+
+//  ES5
+var colors = ['red','blue','green'];
+
+for (var i =0; i < colors.length; i++)
+{
+    console.log(colors[i]);
+}
+
+// ES6
+colors.forEach(function(color){
+    console.log(color)
+})
+
+
+var numbers =[1,2,3,4,5];
+var sum = 0;
+
+function add (number){
+    sum += number;
+}
+
+number.forEach(add);
+
+// ex 
+console.log(sum);
+
+spamMails = [];
+spamMails.forEach(function(spamMail){
+    deleteMail(spamMail);
+})
+
+
+// test 
+function handlePosts(){
+    var posts = [{id:23,title: 'aaaaaaaa'},{id:23,title: 'aaaaaaaa'},{id:23,title: 'aaaaaaaa'}];
+
+    posts.forEach(function(post){
+        savePost(post);
+    })
+}
+
+// teset 2
+var images = [{height: 10 , width:30},{height: 10 , width:30},{height: 10 , width:30}]
+
+var areas = [];
+
+images.forEach(function(imag){
+
+    areas.push(imag.height * imag.width);
+})
+
+console.log(areas);
+
+[].map();
+
+[].filter();
+
+[].find();
+
+[].every();
+
+[].some();
+
+[].reduce();
